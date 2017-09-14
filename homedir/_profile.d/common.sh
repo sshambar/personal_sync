9fab1061cbe78d3ccc62b4eb6ffb05fe4f4c7d58
Scott Shambarger <devel@shambarger.net>
2017-09-14 16:06:30 -0700

    Minor tweaks

--- homedir/_profile.d/common.sh
+++ homedir/_profile.d/common.sh
@@ -1,15 +1,15 @@
 # -*- tab-width: 2; indent-tabs-mode: nil -*- vim:ft=sh:et:sw=2:ts=2:sts=2
 # common.sh - common defines
 
-if [ -n "$PS1" ]; then
+# Skip all for noninteractive shells.
+[ ! -t 0 ] && return
 
-  function scr() {
-    local args
-    if [ "$*" == "" ]; then args="-U -R"; else args="-U $*"; fi
-    script -q -c "screen $args" /dev/null
-  }
+function scr() {
+  local args
+  if [ "$*" == "" ]; then args="-U -R"; else args="-U $*"; fi
+  script -q -c "screen $args" /dev/null
+}
 
-  # nice shortcuts
-  alias h=history
-  alias ll='ls -l'
-fi
+# nice shortcuts
+alias h=history
+
