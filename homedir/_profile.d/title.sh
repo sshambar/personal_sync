3923584e53afce7b34dc2bf6eeb26618734f5e56
Scott Shambarger <devel@shambarger.net>
2018-01-20 18:10:11 -0500

    cleanup title.sh

--- homedir/_profile.d/title.sh
+++ homedir/_profile.d/title.sh
@@ -4,8 +4,8 @@
 # Skip all for noninteractive shells.
 [ ! -t 0 ] && return
 
-# for interactive terminals only
-setup_title() {
+if [ -n "$BASH_VERSION" ]; then
+  PS1="[\u@\h \W]\\$ "
 
   case $TERM in
     xterm*|screen*)
@@ -20,16 +20,9 @@ setup_title() {
     *)
       ;;
   esac
-}
-
-if [ -n "$BASH_VERSION" ]; then
-  PS1="[\u@\h \W]\\$ "
-  setup_title
 else
   PS1="[$USER@\\h \\W]\\$ "
 fi
-
-unset -f setup_title
 # Local Variables:
 # sh-basic-offset: 2
 # indent-tabs-mode: nil
