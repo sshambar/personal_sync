5e8dbfd011351fd1a6fae36fbb080b064123eec5
Scott Shambarger <devel@shambarger.net>
2018-06-12 18:39:56 -0700

    Added colorized man

--- homedir/_profile.d/common.sh
+++ homedir/_profile.d/common.sh
@@ -13,6 +13,16 @@ function scr() {
 # nice shortcuts
 alias h=history 2>/dev/null
 
+# colorize man pages
+export LESS_TERMCAP_mb=$'\E[1;32m'     # begin bold
+export LESS_TERMCAP_md=$'\E[1;32m'     # begin blink
+export LESS_TERMCAP_me=$'\E[0m'        # reset bold/blink
+export LESS_TERMCAP_so=$'\E[01;44;33m' # begin reverse video
+export LESS_TERMCAP_se=$'\E[0m'        # reset reverse video
+export LESS_TERMCAP_us=$'\E[1;4;31m'   # begin underline
+export LESS_TERMCAP_ue=$'\E[0m'        # reset underline
+export GROFF_NO_SGR=1                  # for konsole and gnome-terminal
+
 # Local Variables:
 # sh-basic-offset: 2
 # indent-tabs-mode: nil
