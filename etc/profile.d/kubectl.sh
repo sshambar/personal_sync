d1ca39f8ed6d5376fc565d09e2670d86b8159482
Scott Shambarger <devel@shambarger.net>
2025-11-19 09:07:28 -0800

    profile.d/kubectl.sh: added

---
+++ etc/profile.d/kubectl.sh
@@ -0,0 +1,34 @@
+# -*- mode: sh; sh-basic-offset: 2; indent-tabs-mode: nil; -*-
+# vim:set ft=sh et sw=2 ts=2:
+# shellcheck shell=sh disable=SC3044
+
+# Skip all for noninteractive shells.
+[ ! -t 0 ] && return
+
+[ -x /usr/bin/kubectl ] || return 0
+
+alias kc=kubectl
+if [ -n "$BASH" ]; then
+  _my_kc_load_comp() {
+    declare -F __load_completion >/dev/null || return 0
+    __load_completion kubectl && complete -o default -F __start_kubectl kc
+  }
+  complete -o default -F _my_kc_load_comp kc
+fi
+
+kcns() {
+  _c=$(kubectl config current-context)
+  if [ -n "$_c" ]; then
+    if [ -n "$1" ]; then
+      kubectl config set "contexts.$_c.namespace" "$1"
+    else
+      kubectl config view \
+              -o jsonpath="{.contexts[?(@.name == \"$_c\")].context.namespace}"
+      echo
+    fi
+  else
+    echo >&2 "Unable to find current context"
+  fi
+  unset _c
+}
+
